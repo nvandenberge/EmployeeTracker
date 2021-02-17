47c978fa3f8aa2e90829aa2a46387f0395a59d32
Nick Vandenberge <nvandenberge14@gmail.com>
2021-02-17 13:48:35 -0500

    added sql schema

---
+++ schema.sql
@@ -0,0 +1,35 @@
+-- CREATING 'EMPLOYEES' DATABASE
+DROP DATABASE IF EXISTS employees_DB;
+
+CREATE DATABASE employees_DB;
+
+use employees_DB;
+
+-- DEPARTMENT TABLE --
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- ROLE TABLE --
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+-- EMPLOYEE TABLE --
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+);
